ce4e4679d05ef9cbb2928b2f35e055c38022a34b
yoyos98 <dieeyoyo@gmail.com>
2026-06-17 18:57:03 +0200

    gg

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "euro-finance-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+        .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)
+        ))
+        .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(r => r || fetch(event.request))
+    );
+});
